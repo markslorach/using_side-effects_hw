6da9ef70824a2da170f8567385a9cc6740e5f013
Mark Slorach <hello@markslorach.com>
2023-06-28 18:53:54 +0100

    completed tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+
+/* 
+The number variable is the initial state, and the setNumber function is used to update the state of number.
+The doubleNum variable is initialized to the value of number multiplied by 2. The setDoubleNum function is used to update the state of doubleNum.
+*/
+  const [number, setNumber] = useState(0);
+  const [doubleNum, setDoubleNum] = useState(0)
+
+  useEffect(()=>{
+    console.log("use effect triggered");
+    setDoubleNum(number * 2);
+  },[number])
+
+
+  const handleInc = () => {
+    setNumber(number + 1);
+    // setDoubleNum(number * 2);
+  }
+
+  const handleDec = () => {
+    
+    setNumber(number - 1);
+    // setDoubleNum(number * 2);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <button value={number} onClick={handleInc}> + </button>
+
+      <h2>{number}</h2>
+
+      <h2>{doubleNum}</h2>
+
+      <button value={number} onClick={handleDec}> - </button>
+
     </div>
+
   );
 }
+
+
+
+
 
 export default App;
